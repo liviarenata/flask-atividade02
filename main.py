65ed56495a24e7983b73a9c272e8121e03dfd2d3
Lívia Renata <74880716+liviarenata@users.noreply.github.com>
2022-03-17 23:46:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def ola_mundo():
+  return '<h1>Olá Mundo!</h1>'
+
+@app.route('/unifran/')
+def unifran():
+  return '<h2>Universidade de Franca</h2>'
+  
+if __name__ == ('__main__'):
+  app.run(host='0.0.0.0', port=8080)
